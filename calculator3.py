083f35c013bba3d6a3846f9a4c0b942a996138d4
lebron123123 <116098340+lebron123123@users.noreply.github.com>
2026-03-27 09:32:42 +0800

    Update calculator3.py

--- calculator3.py
+++ calculator3.py
@@ -246,13 +246,13 @@ if ("sale_and_commercial" in current_config.get("ui_components", [])) or ("rent_
         st.subheader("💸 税金及成本计算参数")
         # 第1行：2个参数
         col1, col2 = st.columns(2)
-        land_cost = col1.number_input("土地成本费（万元）", min_value=0.0, value=0.0, step=10.0)
+        land_cost = col1.number_input("(非配售)土地成本费（万元）", min_value=0.0, value=0.0, step=10.0)
         construction_cost = col2.number_input("(非配售)建安工程费（万元）", min_value=0.0, value=0.0, step=10.0)
         st.markdown("")  # 换行
     
         # 第2行：2个参数
         col3, col4 = st.columns(2)
-        infra_cost = col3.number_input("基础设施建设费（万元）", min_value=0.0, value=0.0, step=10.0)
+        infra_cost = col3.number_input("(非配售)基础设施建设费（万元）", min_value=0.0, value=0.0, step=10.0)
         other_eng_cost = col4.number_input("(非配售)工程建设其他费用（万元）", min_value=0.0, value=0.0, step=10.0)
         st.markdown("")  # 换行
     
