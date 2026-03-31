f7c24c9882e55c7fb75fa3afc67039aecd07eb70
lebron123123 <116098340+lebron123123@users.noreply.github.com>
2026-03-31 15:16:56 +0800

    Update calculator3.py

--- calculator3.py
+++ calculator3.py
@@ -1267,11 +1267,6 @@ if calc_button:
     if project_type == "出售类(配保房/可售型人才房等)": income_df_T = income_df.T; income_df_T["全周期合计(万元)"] = income_df_T.apply(lambda r: round(r.sum(),4) if r.name in income_sum_rows else "/", axis=1); income_df_T = income_df_T[["全周期合计(万元)"] + [c for c in income_df_T.columns if c != "全周期合计(万元)"]].fillna("/")
     # 第3行：【核心1行】把指定行强制放到最前面
     if project_type == "出售类(配保房/可售型人才房等)": income_df_T = income_df_T.reindex(["配保房销售收入(万元)", "出租净收益现值(万元)"] + [idx for idx in income_df_T.index if idx not in ["配保房销售收入(万元)", "出租净收益现值(万元)"]])
-    # ===================== 【赋值顶部指标】=====================
-    if project_type == "出售类(配保房/可售型人才房等)":
-        profit_df = calc_profit(all_years, income_df, total_cost_df, tax_df)  # 重算利润表
-        total_income = round(income_df["总收入(万元)"].sum(), 2)  # 重算总收入
-        total_net_profit = round(profit_df["净利润(万元)"].sum(), 2)  # 重算净利润
     
     # --- 收入明细 ---
     st.subheader("📋 收入明细表")
