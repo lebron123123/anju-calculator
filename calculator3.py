fcb3a2218bd22664c9e6c1d025d065515300c45a
lebron123123 <116098340+lebron123123@users.noreply.github.com>
2026-04-22 18:40:39 +0800

    Update calculator3.py

--- calculator3.py
+++ calculator3.py
@@ -1322,10 +1322,7 @@ if calc_button:
         dev_cost_base = total_investment * (sale_area / area_total) if area_total != 0 else 0.0
         # 初始化新列
         cf_df[["开发成本投资(万元)", "销售费用(万元)", "销售税金及附加(万元)", "出租经营税金(万元)", "出租营运成本(万元)", "调整所得税(万元)"]] = 0.0
-        dev_cost_total = dev_cost_base - total_cost_df["财务费用(建设期)(万元)"].sum() - total_cost_df["销售费用(万元)"].sum()
-             
-        # 年度值统一填/，不做逐年计算，仅合计行生效
-        cf_df["开发成本投资(万元)"] = float('nan')
+        
         # 合计计算：严格按你给的公式
         for year in all_years:
             # 一行提取所有现成数据
@@ -1334,6 +1331,11 @@ if calc_button:
             rent_cost = rental_cost_df.loc[year, "出租营运成本合计(万元)"] if year in rental_cost_df.index else 0.0
             build_fin_fee = total_cost_df.loc[year, "财务费用(建设期)(万元)"]
             dev_cost_sale, dev_cost_dep = total_cost_df.loc[year, ["累计开发成本（销售部分）(万元)", "累计开发成本（折旧摊销部分）(万元)"]]
+
+            dev_cost_total = dev_cost_base - total_cost_df["财务费用(建设期)(万元)"].sum() - total_cost_df["销售费用(万元)"].sum()
+             
+            # 年度值统一填/，不做逐年计算，仅合计行生效
+            cf_df["开发成本投资(万元)"] = float('nan')
             
             # 一行计算调整所得税（负数自动取0）
             adjust_tax = max( (cf_df.loc[year, "现金流入(万元)"] - cf_df.loc[year, "回收固定资产余值(万元)"] - (dev_cost_sale + dev_cost_dep + sale_fee + sale_tax + rent_cost + rent_tax)) * 0.25, 0.0 )
